dfcb04dd2ac11cf68e8d9e6b4a801f804a911da8
gache <erickfrancodelgado@hotmail.com>
2020-12-15 12:08:58 +0100

    carrection du form et app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// imporation pour travailler avec le formulaire
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// Importation pour faire petitions Http
+import { HttpClientModule } from '@angular/common/http';
 // Importation de la route
 import { AppRoutingModule } from './app-routing.module';
 
-// Importation des modules
 import { NgsRevealModule } from 'ngx-scrollreveal';
 import { NgsRevealConfig } from 'ngx-scrollreveal';
-import { HttpClientModule } from '@angular/common/http';
+
+// Importation des Composannts
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ExperiencesComponent } from './components/experiences/experiences.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { PropoMoiComponent } from './components/propo-moi/propo-moi.component';
-import { DetailExperienceComponent } from './components/detailExperience/detail-experience.component';
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { ReseauxComponent } from './components/reseaux/reseaux.component';
-import { LangageInforComponent } from './components/langage-infor/langage-infor.component';
 import { ProjetsComponent } from './components/projets/projets.component';
 import { DiplomesComponent } from './components/diplomes/diplomes.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LangageInforComponent } from './components/langage-infor/langage-infor.component';
+import { DetailExperienceComponent } from './components/detailExperience/detail-experience.component';
+
 
 @NgModule( {
   declarations: [
@@ -45,7 +48,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     NgsRevealModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+
   ],
   providers: [
     NgsRevealConfig,
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -26,6 +26,8 @@ export class ContactComponent implements OnInit {
 
     email: new FormControl( '', [Validators.required, Validators.pattern( /^[a-zA-Z0-9]{2,150}/ )] ),
 
+    telephone: new FormControl( '', [Validators.required] ),
+
     sujet: new FormControl( '', [Validators.required, Validators.minLength( 3 )] ),
 
     message: new FormControl( '', [Validators.required, Validators.minLength( 5 )] ),
@@ -38,25 +40,32 @@ export class ContactComponent implements OnInit {
   }
 
   afficherValeur () {
+
     let contact: ContactInterfacer = {
       id: null,
       email: this.contactForm.get( 'email' ).value,
       nom: this.contactForm.get( 'nom' ).value,
       prenom: this.contactForm.get( 'prenom' ).value,
+      telephone: this.contactForm.get( 'telephone' ).value,
       sujet: this.contactForm.get( 'sujet' ).value,
       message: this.contactForm.get( 'message' ).value
     }
     console.log( contact );
 
     this.contactService.registerMessage( contact ).subscribe( res => {
+
       console.log( res );
     } );
 
+    this.contactForm.reset();
+
+
   }
 
-  get title (): AbstractControl {
-    return this.contactForm.get( 'title' );
+  clearFormulaire () {
+    this.contactForm.reset();
   }
+
 
 
 }
